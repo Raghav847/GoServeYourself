d3a43a2c603dbc3c71b6f243fe54d1254966bdcb
Raghav Khandelwal <raaghav.khandelwal@gmail.com>
2026-05-20 15:23:29 -0700

    URL params

--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"fmt"
+	"bytes"
 	"log"
 	"log/slog"
 	"net/http"
@@ -10,27 +10,47 @@ import (
 func main() {
 	mux := http.NewServeMux()
 
-	mux.HandleFunc("/", handleHello)
+	mux.HandleFunc("/", handleRoot)
 	mux.HandleFunc("/goodbye", handleGoodbye)
+	mux.HandleFunc("/hello/", handleHelloParameterized)
 	log.Fatal(http.ListenAndServe(":8080", mux))
 }
 
-func handleHello(w http.ResponseWriter, _ *http.Request) {
-	wc, err := w.Write([]byte("Hello, World!\n"))
+func handleRoot(w http.ResponseWriter, _ *http.Request) {
+	_, err := w.Write([]byte("HomePage!\n"))
 	if err != nil {
 		slog.Error("error writing response", "err", err)
 		return
 	}
 
-	fmt.Printf("%d bytes written\n", wc)
 }
 
 func handleGoodbye(w http.ResponseWriter, _ *http.Request) {
-	wc, err := w.Write([]byte("Goodbye!\n"))
+	_, err := w.Write([]byte("Goodbye!\n"))
 	if err != nil {
 		slog.Error("error writing response", "err", err)
 		return
 	}
 
-	fmt.Printf("%d bytes written\n", wc)
+}
+
+func handleHelloParameterized(w http.ResponseWriter, r *http.Request) {
+	params := r.URL.Query()
+	userList := params["user"]
+
+	userName := "User"
+	if len(userList) > 0 {
+		userName = userList[0]
+	}
+
+	var output bytes.Buffer
+	output.WriteString("Hello, ")
+	output.WriteString(userName)
+	output.WriteString("!\n")
+
+	_, err := w.Write(output.Bytes())
+	if err != nil {
+		slog.Error("error writing response body", "err", err)
+		return
+	}
 }
--- main_test.go
+++ main_test.go
@@ -7,17 +7,17 @@ import (
 	"testing"
 )
 
-func TestHandleHello(t *testing.T) {
+func TestHandleRoot(t *testing.T) {
 	w := httptest.NewRecorder()
 
-	handleHello(w, nil)
+	handleRoot(w, nil)
 
 	desiredCode := http.StatusOK
 	if w.Code != desiredCode {
 		t.Errorf("bad response code, expected %v got %v\nbody: %s\n", desiredCode, w.Code, w.Body.String())
 	}
 
-	expectedMessage := []byte("Hello, World!\n")
+	expectedMessage := []byte("HomePage!\n")
 	if !bytes.Equal(expectedMessage, w.Body.Bytes()) {
 		t.Errorf("bad return, got: %q, expected: %q", w.Body.String(), expectedMessage)
 	}
@@ -34,6 +34,60 @@ func TestHandleGoodbye(t *testing.T) {
 	}
 
 	expectedMessage := []byte("Goodbye!\n")
+	if !bytes.Equal(expectedMessage, w.Body.Bytes()) {
+		t.Errorf("bad return, got: %q, expected: %q", w.Body.String(), expectedMessage)
+	}
+}
+
+func TestHandleHelloParameterized(t *testing.T) {
+	req := httptest.NewRequest(http.MethodGet, "/hello?user=TestMan", nil)
+
+	w := httptest.NewRecorder()
+
+	handleHelloParameterized(w, req)
+
+	desiredCode := http.StatusOK
+	if w.Code != http.StatusOK {
+		t.Errorf("bad response code, expected %v got %v\nbody: %s\n", desiredCode, w.Code, w.Body.String())
+	}
+
+	expectedMessage := []byte("Hello, TestMan!\n")
+	if !bytes.Equal(expectedMessage, w.Body.Bytes()) {
+		t.Errorf("bad return, got: %q, expected: %q", w.Body.String(), expectedMessage)
+	}
+}
+
+func TestHandleHelloParameterizedNoParam(t *testing.T) {
+	req := httptest.NewRequest(http.MethodGet, "/hello/", nil)
+
+	w := httptest.NewRecorder()
+
+	handleHelloParameterized(w, req)
+
+	desiredCode := http.StatusOK
+	if w.Code != http.StatusOK {
+		t.Errorf("bad response code, expected %v got %v\nbody: %s\n", desiredCode, w.Code, w.Body.String())
+	}
+
+	expectedMessage := []byte("Hello, User!\n")
+	if !bytes.Equal(expectedMessage, w.Body.Bytes()) {
+		t.Errorf("bad return, got: %q, expected: %q", w.Body.String(), expectedMessage)
+	}
+}
+
+func TestHandleHelloParameterizedWrongParam(t *testing.T) {
+	req := httptest.NewRequest(http.MethodGet, "/hello?foo=bar", nil)
+
+	w := httptest.NewRecorder()
+
+	handleHelloParameterized(w, req)
+
+	desiredCode := http.StatusOK
+	if w.Code != http.StatusOK {
+		t.Errorf("bad response code, expected %v got %v\nbody: %s\n", desiredCode, w.Code, w.Body.String())
+	}
+
+	expectedMessage := []byte("Hello, User!\n")
 	if !bytes.Equal(expectedMessage, w.Body.Bytes()) {
 		t.Errorf("bad return, got: %q, expected: %q", w.Body.String(), expectedMessage)
 	}
